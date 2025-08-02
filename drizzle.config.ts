c41b4c0f8ae340a497cdf189ca392a51423d021f
Prakash Raut <prakash_raut@outlook.com>
2025-08-02 17:57:36 +0530

    refactor: update database URL configuration to use new environment variable structure

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,12 @@
 import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
-import { Config } from "./config/env";
+import { env } from "./config/env";
 
 export default defineConfig({
 	schema: "./db/schema.ts",
 	out: "./migrations",
 	dialect: "postgresql",
 	dbCredentials: {
-		url: Config.DATABASE_URL,
+		url: env.DATABASE_URL,
 	},
 });
